67972aeac985c2a86609b6d0e482b903dae2c83a
ilbw97 <ilbw97@naver.com>
2021-08-17 18:51:53 +0900

    test

--- server/server.go
+++ server/server.go
@@ -10,6 +10,9 @@ import (
 	"strconv"
 )
 
+type data struct {
+}
+
 func main() {
 	var (
 		network = "tcp"
@@ -28,22 +31,49 @@ func main() {
 			log.Printf("Accept Error : %v\n", err)
 			continue
 		}
-		go ConnHandler(conn) //연결을 parameter로 넘겨주고 ConnHandler go routine 실행 (goroutine 실행 이유 : client는 여러개일수도 있으니까.)
+		// recvall := make(chan []byte)
+		// recvlen := make(chan int, 0)
+		// totsize := make(chan int, 0)
+		// go CommRead(conn, recvall, recvlen, totsize) //연결을 parameter로 넘겨주고 ConnHandler go routine 실행 (goroutine 실행 이유 : client는 여러개일수도 있으니까.)
+		go CommRead(conn)
+		// sendingall := <-recvall
+		// sendinglen := <-recvlen
+		// sendingsize := <-totsize
+		// log.Printf("sendingall : %v\n", sendingall)
+		// log.Printf("sendinglen : %v\n", sendinglen)
+		// log.Printf("sendingsize : %v\n", sendingall)
+
+		// go CommSend(conn, sendingall, sendinglen, sendingsize)
+
 	}
 }
 
 var ErrorConfirmData = errors.New("")
 
-func ConnHandler(conn net.Conn) {
+// func CommSend(conn net.Conn, sendingall []byte, sendinglen int, sendingsize int) {
+// 	defer conn.Close()
+// 	// sendingall = sendingall
+// 	// resultlarge := execute(string(sendingall[:sendinglen]))
+// 	go execute(string(sendingall)[:sendinglen])
+// 	_, err := conn.Write(resultlarge)
+// 	if err != nil {
+// 		log.Printf("write err : %v\n", err)
+// 		// return
+// 	}
+// 	log.Printf("resultlen : %v == totsize : %v\n", sendinglen, sendingsize)
+// }
 
+func CommRead(conn net.Conn) {
 	log.Printf("serving %s\n", conn.RemoteAddr().String())
 	defer conn.Close()
 	for {
 		var recvall []byte
+		// recvall := make(chan []byte)
+		// recvlen := make(chan int, 0)
 		recvlen := 0
 		recvData := make([]byte, 4096)          //값을 읽어와 저장할 버퍼 생성
 		recvCommand, err := conn.Read(recvData) //client가 값을 줄 때까지 blocking 되어 대기하다가 값을 주면 읽어들인다.
-
+		resultlarge := make(chan []byte)
 		if nil != err { //입력이 종료되면 종료
 			if io.EOF == err {
 				log.Printf("connection is closed from client : %v\n", conn.RemoteAddr().String())
@@ -94,41 +124,37 @@ func ConnHandler(conn net.Conn) {
 							a += 1
 							log.Printf("%d번째 loop\n", a)
 							if recvlen == totsize {
-								go func() {
-									resultlarge := execute(string(recvall[:recvlen]))
-									_, err := conn.Write(resultlarge)
-									if err != nil {
-										log.Printf("write err : %v\n", err)
-										return
-									}
-									log.Printf("resultlen : %v == totsize : %v\n", recvlen, totsize)
-								}()
-								// resultlarge := execute(string(recvall[:recvlen]))
-								// _, err := conn.Write(resultlarge)
-								// if err != nil {
-								// 	log.Printf("write err : %v\n", err)
-								// 	return
-								// }
-								// log.Printf("resultlen : %v == totsize : %v\n", recvlen, totsize)
+								// sendingall <- recvall
+								// sendinglen <- recvlen
+								// sendingsize <- totsize
+								go execute(string(recvall[:recvlen]), resultlarge)
+								sending := <-resultlarge
+								_, err := conn.Write(sending)
+								if err != nil {
+									log.Printf("write err : %v\n", err)
+									return
+								}
+								log.Printf("resultlen : %v == totsize : %v\n", recvlen, totsize)
 							}
+							// return
 							log.Printf("NOT FINISH LOOP resultlen : %v == totsize : %v\n", recvlen, totsize)
 						}
+
 					} else {
-						go func() {
-							result := execute(string(recvall[:recvlen]))
-							_, err := conn.Write(result)
-							if err != nil {
-								log.Printf("write err : %v\n", err)
-								return
-							}
-						}()
-						// result := execute(string(recvall[:recvlen]))
-						// _, err := conn.Write(result)
-						// if err != nil {
-						// 	log.Printf("write err : %v\n", err)
-						// 	return
-						// }
+						// sendingall <- recvall
+						// sendinglen <- recvlen
+						// sendingsize <- totsize
+						go execute(string(recvall[:recvlen]), resultlarge)
+						sending := <-resultlarge
+						_, err := conn.Write(sending)
+						if err != nil {
+							log.Printf("write err : %v\n", err)
+							return
+						}
+						log.Printf("resultlen : %v == totsize : %v\n", recvlen, totsize)
 					}
+
+					// return
 				}
 			} else {
 				log.Println("No receive data")
@@ -139,8 +165,7 @@ func ConnHandler(conn net.Conn) {
 	}
 }
 
-func execute(command string) []byte {
-
+func execute(command string, resultlarge chan []byte) {
 	cmd := exec.Command("bash", "-c", command)
 	log.Printf("Execute Command : %v\n", cmd)
 
@@ -149,15 +174,15 @@ func execute(command string) []byte {
 	if err != nil {
 		log.Println(err)
 		cmdreslen = []byte(strconv.Itoa(len("Command error : "+err.Error()+"\n")) + "\n")
-		return append(cmdreslen, []byte("Command error : "+err.Error())...)
+		resultlarge <- append(cmdreslen, []byte("Command error : "+err.Error())...)
 	}
 	if string(cmdres) == "" {
 		cmdreslen = []byte(strconv.Itoa(len("No output data"+"\n")) + "\n")
-		return append(cmdreslen, ([]byte("No output data"))...)
+		resultlarge <- append(cmdreslen, ([]byte("No output data"))...)
 	}
 
 	log.Printf("stdout: %v bytes\n%s", string(bytes.Trim(cmdreslen, "\n")), string(cmdres))
 
-	return append(cmdreslen, cmdres...)
-
+	resultlarge <- append(cmdreslen, cmdres...)
+	// return append(cmdreslen, cmdres...)
 }
