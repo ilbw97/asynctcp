07d59fa844b55355b3cd2e1a3f504e9cb4ae31ba
ilbw97 <ilbw97@naver.com>
2021-08-25 16:54:57 +0900

    fail

--- client/client.go
+++ client/client.go
@@ -9,21 +9,20 @@ import (
 	"net"
 	"os"
 	"strconv"
-	"sync"
 )
 
-var ErrorNocommand = errors.New("insert command!")
+var ErrorNocommand = errors.New("Insert command!")
 
 var ErrorNotEnded = errors.New("")
-var ErrorNotConverttoAtoi = errors.New("")
 
 var ErrorReadLargeReceive = errors.New("Cannot Read Large Data")
-var ErrorEOF = errors.New("연결 종료")
+var ErrorEOF = errors.New("Connection is closed from server")
 
-var lock = new(sync.Mutex)
+// var lock = new(sync.Mutex)
 
 var connerr error
 
+// Server가 보낸 data를 읽어 log로 출력하는 funciton
 func read(c net.Conn) error {
 
 	for { //연결이 존재할 때
@@ -31,85 +30,58 @@ func read(c net.Conn) error {
 		resultlen := 0
 		data := make([]byte, 4096)
 
-		lock.Lock()
-		log.Println("lock")
 		receive, err := c.Read(data) //server로부터 data 읽어오면
-		lock.Unlock()
-		log.Println("unlock")
+
 		if err != nil {
 			if err == io.EOF {
-				connerr = ErrorEOF
+				connerr = ErrorEOF //conn error 공유
 				return ErrorEOF
 			}
+
 			return err
 		} else { //data를 읽어올 때 error 없을 경우
 			if receive > 0 {
 				recvSize := data[:bytes.Index(data, []byte("\n"))] //data 첫부분에서 보낸 size check
 				size, err := strconv.Atoi(string(recvSize))        //int로 converting
 
-				log.Printf("receive size : %d\n", size)
-
-				lock.Lock()
-				sizelen := len(strconv.Itoa(size)) //server가 명시한 data의 전체 길이(cmd.Output()의 길이)
-				log.Printf("sizelen : %d\n", sizelen)
-				lock.Unlock()
-
-				totsize := size + sizelen //server가 보낸 data의 전체 길이
-				log.Printf("totsize : %d\n", totsize)
 				if err != nil {
 					panic(err)
 				} else {
+					sizelen := len(strconv.Itoa(size)) //server가 명시한 data의 전체 길이(cmd.Output()의 길이)
 
+					totsize := size + sizelen //server가 보낸 data의 전체 길이
 					sizeindex := bytes.Index(data, []byte("\n")) + 1
-					log.Printf("sizeindex : %d\n", sizeindex)
 
-					log.Printf("receive - sizeindex : %d\n", receive-sizeindex)
-
-					lock.Lock()
 					resultall = append(resultall, data[sizeindex:receive]...)
 					resultlen += receive
-					lock.Unlock()
-					log.Println("unlock")
 
 					if totsize > len(data) {
 						a := 0
 						for {
 							log.Println("loop started")
-							lock.Lock()
 							LargeReceive, err := c.Read(data)
-							lock.Unlock()
 
 							if err != nil {
-								return ErrorReadLargeReceive
+								return err
 							}
 							log.Printf("Read LargeReceive : %v\n", LargeReceive)
 
-							lock.Lock()
-							log.Println("lock")
 							resultlen += LargeReceive
 							resultall = append(resultall, data[:LargeReceive]...)
-							lock.Unlock()
-							log.Println("unlock")
 
 							a += 1
 							log.Printf("%d번째 loop\n", a)
 							if resultlen == totsize {
-								lock.Lock()
-								log.Println("lock")
 								log.Printf("\n%v", string(resultall[:resultlen]))
 								log.Printf("resultlen : %v == totsize : %v\n", resultlen, totsize)
-								lock.Unlock()
-								log.Println("unlock")
+
 								break
 							}
 							log.Printf("NOT FINISH LOOP resultlen : %v != totsize : %v\n", resultlen, totsize)
 						}
 					} else {
-						lock.Lock()
-						log.Println("lock")
 						log.Printf("\n%v", string(resultall[:receive-sizeindex]))
-						lock.Unlock()
-						log.Println("unlock")
+
 						return nil
 					}
 					return nil
@@ -123,17 +95,20 @@ func read(c net.Conn) error {
 	}
 }
 
+// Stdin으로 입력받은 data를 Server에게 전송하는 function
 func sending(c net.Conn, sendingerr chan error) {
 
 	sc := bufio.NewScanner(os.Stdin) //init scanner
 
-	sc.Scan() //stdinput으로 들어온 한 줄 그대로 scan
+	sc.Scan()
 	if sc.Err() != nil {
 		sendingerr <- sc.Err()
 		return
 	} else {
-		var comm string  //command
-		comm = sc.Text() //읽어온 데이터를 변수에 저장
+		var comm string
+		comm = sc.Text()
+
+		//connection error 존재 시
 		if connerr != nil {
 			sendingerr <- connerr
 		} else {
@@ -163,7 +138,7 @@ func main() {
 		log.Println(err)
 		return
 	}
-	log.Println("Read goroutine make")
+
 	go func() {
 		for {
 			readerr := read(conn)
@@ -187,7 +162,6 @@ func main() {
 		defer conn.Close()
 		sendingerr := make(chan error)
 
-		log.Println("Sending goroutine make")
 		go sending(conn, sendingerr)
 
 		e := <-sendingerr
